7bfb2e3fb51919ee6bb6a70c78d211ce6a0d7b65
Dominik Możdżeń <dominikmozdzen@gmail.com>
2024-06-08 10:39:24 +0200

    Dodaj pliki projektów.

---
+++ komivoyager/Program.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        using (var reader = new StreamReader("dane.csv"))
+        {
+            int[,] data = new int[100,100];
+            for (int i = 0; i < 100; i++)
+            {
+                var line = reader.ReadLine().Split(';');
+                for(int j = 0; j < line.Length; j++)
+                    data[i,j] = int.Parse(line[j]);
+            }
+        }
+    }
+}
