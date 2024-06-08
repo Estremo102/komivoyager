7eb4102758f7be29a5e451dd0b388fa7a95b49a5
Dominik Możdżeń <dominikmozdzen@gmail.com>
2024-06-08 10:43:17 +0200

    Read Data

--- komivoyager/Program.cs
+++ komivoyager/Program.cs
@@ -6,15 +6,16 @@ class Program
 {
     static void Main()
     {
+        int[,] data = new int[100, 100];
         using (var reader = new StreamReader("dane.csv"))
         {
-            int[,] data = new int[100,100];
             for (int i = 0; i < 100; i++)
             {
-                var line = reader.ReadLine().Split(';');
-                for(int j = 0; j < line.Length; j++)
-                    data[i,j] = int.Parse(line[j]);
+                string[] line = reader.ReadLine().Split(';');
+                for (int j = 0; j < line.Length; j++)
+                    data[i, j] = int.Parse(line[j]);
             }
         }
+
     }
 }
