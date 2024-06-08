26aaa6eb007c45250f79e3d9d6c6afb248a1aac1
Dominik Możdżeń <dominikmozdzen@gmail.com>
2024-06-08 13:01:59 +0200

    fixing

--- komivoyager/Program.cs
+++ komivoyager/Program.cs
@@ -7,7 +7,8 @@ class Program
     static void Main()
     {
         //config
-        int CountOfPopulation = 1000;
+        int countOfPopulation = 1000;
+        int mutationPercent = 3; //in %
         //Read data from csv
         int[,] data = new int[100, 100];
         using (var reader = new StreamReader("dane.csv"))
@@ -20,28 +21,40 @@ class Program
             }
         }
         //Initialize Random Population
-        int[][] travelOrderPopulation = new int[CountOfPopulation][];
+        int[][] travelOrderPopulation = new int[countOfPopulation][];
         for (int i = 0; i < travelOrderPopulation.Length; i++)
         {
             travelOrderPopulation[i] = new int[100];
             for (int j = 0; j < travelOrderPopulation[i].Length; j++)
-                travelOrderPopulation[i][j] = j;
+                travelOrderPopulation[i][j] = 99-j;
             Random random = new Random();
-            for (int j = 0; j < travelOrderPopulation[i].Length; j++)
+            for (int j = 0; j < travelOrderPopulation[i].Length-1; j++)
             {
-                int k = random.Next(100);
+                int k = random.Next(99);
                 int tmp = travelOrderPopulation[i][j];
                 travelOrderPopulation[i][j] = travelOrderPopulation[i][k];
                 travelOrderPopulation[i][k] = tmp;
             }
         }
-        Solution[] solutions = new Solution[CountOfPopulation];
+        Solution[] solutions = new Solution[countOfPopulation];
         for (int i = 0; i < solutions.Length; i++)
             solutions[i] = new Solution(travelOrderPopulation[i], data);
         Array.Sort(solutions);
+        //choose best 10% and create children
+        int tenPercent = countOfPopulation / 10;
+        Solution[] newGeneration = new Solution[countOfPopulation];
+        for (int i = 0; i < tenPercent; i++)
+        {
+            newGeneration[i] = solutions[i];
+        }
+        for(int i = tenPercent; i < newGeneration.Length; i++)
+        {
+            newGeneration[i] = newGeneration[newGeneration.Length/i].CreateChild(mutationPercent);
+        }
+        solutions = newGeneration;
         foreach (Solution solution in solutions)
             Console.WriteLine(solution);
-    }
+        }
 
     public static int CostOfTravel(int[] travel, int[,] data, int index)
     {
--- komivoyager/Solution.cs
+++ komivoyager/Solution.cs
@@ -2,23 +2,51 @@
 {
     public int Cost { get; set; }
     public string Path { get; set; }
+    private int[,] data;
 
     public Solution(int[] travel, int[,] data)
     {
-        Path = "Path: ";
-        for(int i = 0; i < travel.Length-1; i++)
+        Path = $"{data[0, travel[0]]};";
+        Cost = data[0, travel[0]];
+        for (int i = 0; i < travel.Length - 1; i++)
         {
             Path += $"{travel[i]};";
-            Cost+= data[travel[i],travel[i+1]];
+            Cost += data[travel[i], travel[i + 1]];
+            this.data = data;
         }
+        Cost += data[travel[travel.Length - 1], 0];
+        Path += $"{data[travel[travel.Length - 1], 0]}";
     }
 
-    public override string ToString() => $"Cost: {Cost}\n{Path}";
+    public override string ToString() => $"Cost: {Cost}\nPath: {Path}";
 
     public int CompareTo(Solution? other)
     {
-        if(other.Cost > Cost) return -1 ;
+        if (other.Cost > Cost) return -1;
         if (other.Cost < Cost) return 1;
-        return 0 ;
+        return 0;
+    }
+
+    public Solution CreateChild(int mutationpercent = 3)
+    {
+        //copy DNA
+        string[] path = Path.Split(';');
+        int[] intPath = new int[path.Length];
+        for (int i = 0; i < intPath.Length; i++)
+        {
+            intPath[i] = int.Parse(path[i]);
+        }
+        //mutation
+        Random random = new Random();
+        for (int i = 0; i < path.Length * mutationpercent / 100; i++)
+        {
+            int a = random.Next(100);
+            int b = random.Next(100);
+            int tmp = intPath[a];
+            intPath[a] = intPath[b];
+            intPath[b] = tmp;
+        }
+        //return child
+        return new Solution(intPath, data);
     }
 }
