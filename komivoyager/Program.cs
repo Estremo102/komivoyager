2d95f87d2cee8af3277a1abffd745b118ce21461
Dominik Możdżeń <dominikmozdzen@gmail.com>
2024-06-08 11:46:05 +0200

    Separate Files

--- komivoyager/Program.cs
+++ komivoyager/Program.cs
@@ -50,27 +50,3 @@ class Program
     }
 }
 
-public class Solution : IComparable<Solution>
-{
-    public int Cost { get; set; }
-    public string Path { get; set; }
-
-    public Solution(int[] travel, int[,] data, int index)
-    {
-        Path = "Path: ";
-        foreach (int i in travel)
-        {
-            Cost += data[index,i];
-            Path += $"{data[index, i]};";
-        }
-    }
-
-    public override string ToString() => $"Cost: {Cost}\n{Path}";
-
-    public int CompareTo(Solution? other)
-    {
-        if(other.Cost > Cost) return -1 ;
-        if (other.Cost < Cost) return 1;
-        return 0 ;
-    }
-}
